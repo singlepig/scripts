8c80dee0cbe595a6f77561a610ed69c02883f047
singlepig <gys.nanjing@gmail.com>
2016-04-10 20:57:45 +0800

    set singlepig.sh as project entrance

---
+++ singlepig.sh
@@ -0,0 +1,10 @@
+# Personal zsh config for singlepig, Hope useful to you!
+# To use this project, add the following lines into ~/.zshrc
+#       cd path to scripts dir
+#       source singlepig.sh
+
+source ./alias.sh
+
+source ./paths.sh
+
+cd - > /dev/null
