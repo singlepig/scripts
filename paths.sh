831061df847bfa2fc72f8fbb32c219d4b2aeb4dc
singlepig <gys.nanjing@gmail.com>
2017-02-15 13:39:45 +0800

    add path for golang

--- paths.sh
+++ paths.sh
@@ -5,5 +5,10 @@
 export ANDROID_SDK_PATH="`echo ~`/Library/Android/sdk"
 export PATH="$PATH:$ANDROID_SDK_PATH/build-tools:$ANDROID_SDK_PATH/platform-tools:$ANDROID_SDK_PATH/tools"
 
+## for go
+export GOROOT=/usr/local/go
+export PATH=$PATH:$GOROOT/bin
+export GOPATH=$HOME/work/go
+
 ## for tools dir
 export PATH="$PATH:`pwd`/tools"
